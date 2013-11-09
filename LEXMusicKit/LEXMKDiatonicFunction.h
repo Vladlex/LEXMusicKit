127b9b9657c53f098b0561b11a969f84f78565eb
Vladlex <vladlex@itechdevs.com>
2013-11-09 16:32:47 +1100

    ‘Create’ word in functions replaced by ‘Make’. ‘Create’ usually points to allocating memory in method implementation. Also ‘ranges’ replaced by ’intervals’ everywhere. The ‘interval’ is the right term in music theory.

--- LEXMusicKit/LEXMKDiatonicFunction.h
+++ LEXMusicKit/LEXMKDiatonicFunction.h
@@ -14,23 +14,25 @@
 #include <stdbool.h>
 #include "LEXMKMode.h"
 
-#define LEXMKDFCreate(i1,i2,i3,i4,i5,i6,i7) LEXMKDiatonicFunctionCreateWithIntervals(i1,i2,i3,i4,i5,i6,i7)
+#define LEXMKDFuncMake(i1,i2,i3,i4,i5,i6,i7) LEXMKDiatonicFunctionMakeWithIntervals(i1,i2,i3,i4,i5,i6,i7)
 
-/// The 'diatonic function' or 'chord area' - is mask of ranges from one degree to another. The value of range is number of halftones from given degree to the next. I. e. for natural major function[0] will be '2'. This is not very good way to keep info but I can't find another way.
-typedef struct LEXMKDiatonicFunction {int ranges[7];} LEXMKDiatonicFunction;
+/// The 'diatonic function' or 'chord area' - is mask of intervals from one degree to another. The value of intervals is number of halftones from given degree to the next. I. e. for natural major function[0] will be '2'. This is not very good way to keep info but I can't find another way.
+typedef struct LEXMKDiatonicFunction {int intervals[7];} LEXMKDiatonicFunction;
 
 
-LEXMKDiatonicFunction LEXMKDiatonicFunctionCreateWithUncompletedIntervals(int i1, int i2, int i3, int i4, int i5, int i6);
-LEXMKDiatonicFunction LEXMKDiatonicFunctionCreateWithIntervals(int i1, int i2, int i3, int i4, int i5, int i6, int i7);
+LEXMKDiatonicFunction LEXMKDiatonicFunctionMakeWithUncompletedIntervals(int i1, int i2, int i3, int i4, int i5, int i6);
+LEXMKDiatonicFunction LEXMKDiatonicFunctionMakeWithIntervals(int i1, int i2, int i3, int i4, int i5, int i6, int i7);
 
-LEXMKDiatonicFunction LEXMKDiatonicFunctionCreateWithUncompletedIntervalVector(const int * ranges);
-LEXMKDiatonicFunction LEXMKDiatonicFunctionCreateWithIntervalVector(const int * ranges);
+LEXMKDiatonicFunction LEXMKDiatonicFunctionMakeWithUncompletedIntervalVector(const int * intervals);
+LEXMKDiatonicFunction LEXMKDiatonicFunctionMakeWithIntervalVector(const int * intervals);
 
 /** Easy way to create diatonic functions by mode.
  @param LEXMusicMode An mode constant. See valid constants in LEXMusicMode enum.
  @return An usable LEXMKDiatonicFunction variable in case of valid mode and LEXMKDiatonicFunctionNull if mode unknown.
  */
-LEXMKDiatonicFunction LEXMKDiatonicFunctionCreateWithMode(LEXMKMode mode);
+LEXMKDiatonicFunction LEXMKDiatonicFunctionMakeWithMode(LEXMKMode mode);
+
+int LEXMKDiatonicFunctionDifferentIntervalIndexInFunctions(LEXMKDiatonicFunction func1, LEXMKDiatonicFunction func2);
 
 bool LEXMKDiatonicFunctionIsEqualToFunction(LEXMKDiatonicFunction func1, LEXMKDiatonicFunction func2);
 
@@ -40,7 +42,7 @@ bool LEXMKDiatonicFunctionIsEqualToFunction(LEXMKDiatonicFunction func1, LEXMKDi
  */
 bool LEXMKDiatonicFunctionIsValid(LEXMKDiatonicFunction function);
 
-/** A function constant with all zero-ranges.
+/** A function constant with all zero-intervals.
  */
 extern const LEXMKDiatonicFunction LEXMKDiatonicFunctionNull;
 
