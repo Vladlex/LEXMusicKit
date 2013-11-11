76236736dabb3a8a80285fe44613bcf34d968a46
Vladlex <vladlex@itechdevs.com>
2013-11-11 22:14:35 +1100

    Initial commit

---
+++ LEXMusicKit/LEXMKChordScheme.c
@@ -0,0 +1,9 @@
+//
+//  LEXMKChordScheme.c
+//  LEXMusicKit
+//
+//  Created by Alexei Gordeev on 11.11.13.
+//  Copyright (c) 2013 Alexei Gordeev. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ LEXMusicKit/LEXMKChordScheme.h
@@ -0,0 +1,17 @@
+//
+//  LEXMKChordScheme.h
+//  LEXMusicKit
+//
+//  Created by Alexei Gordeev on 11.11.13.
+//  Copyright (c) 2013 Alexei Gordeev. All rights reserved.
+//
+
+#ifndef LEXMusicKit_LEXMKChordScheme_h
+#define LEXMusicKit_LEXMKChordScheme_h
+
+#include "LEXMKInterval.h"
+
+typedef struct LEXMKChordScheme {LEXMKInterval intervals *} LEXMKChordScheme;
+
+
+#endif
