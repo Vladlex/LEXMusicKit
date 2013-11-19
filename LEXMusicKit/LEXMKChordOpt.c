32bb89abeac7c03d1631375dbb54fb20e88a59eb
Vladlex <vladlex@itechdevs.com>
2013-11-19 19:12:34 +1100

    Initial commit

---
+++ LEXMusicKit/LEXMKChordOpt.c
@@ -0,0 +1,66 @@
+//
+//  LEXMKChordOpt.c
+//  LEXMusicKit
+//
+//  Created by Alexei Gordeev on 19.11.13.
+//  Copyright (c) 2013 Alexei Gordeev. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <memory.h>
+#include "LEXMKChordOpt.h"
+
+LEXMKChordOpt LEXMKChordOptMakeWithTypeAndInfo(LEXMKChordOptType type, int info)
+{
+    LEXMKChordOpt opt;
+    opt.type = type;
+    opt.info = info;
+    return opt;
+}
+
+bool LEXMKChordOptHasOptWithTypeAndInfo(LEXMKChordOpt *opts,
+                                        unsigned int length,
+                                        LEXMKChordOptType type,
+                                        int info)
+{
+    if (opts != NULL && length > 0) {
+        for (int i = 0; i < length; i++) {
+            LEXMKChordOpt opt = opts[i];
+            if (opt.type == type && opt.info == info) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+LEXMKChordOpt *LEXMKChordCreateOptsWithTypeFromOpts(LEXMKChordOpt * opts,
+                                                    unsigned int length,
+                                                    LEXMKChordOptType type,
+                                                    unsigned int * outFoundOptsLength)
+{
+    if (opts != NULL && length > 0) {
+        unsigned int optsLength = 0;
+        LEXMKChordOpt *resultOpts = malloc(sizeof(LEXMKChordOpt) * 10);
+        LEXMKChordOpt opt;
+        for (int i = 0 ; i< length; i++) {
+            opt = opts[i];
+            if (opt.type == type) {
+                resultOpts[optsLength] = opt;
+                optsLength +=1;
+            }
+        }
+        if (optsLength > 0) {
+            *outFoundOptsLength = optsLength;
+            resultOpts = realloc(resultOpts, sizeof(LEXMKChordOpt) * optsLength);
+            return resultOpts;
+        }
+        else {
+            free(resultOpts);
+        }
+    }
+    *outFoundOptsLength = 0;
+    return NULL;
+}
---
+++ LEXMusicKit/LEXMKChordOpt.h
@@ -0,0 +1,49 @@
+//
+//  LEXMKChordOpt.h
+//  LEXMusicKit
+//
+//  Created by Alexei Gordeev on 19.11.13.
+//  Copyright (c) 2013 Alexei Gordeev. All rights reserved.
+//
+
+#ifndef LEXMusicKit_LEXMKChordOpt_h
+#define LEXMusicKit_LEXMKChordOpt_h
+
+
+enum {
+    LEXMKChordOptTypeUnknown = -1,
+    LEXMKChordOptTypeNotFound = 0,
+    LEXMKChordOptTypeWide,
+    LEXMKChordOptTypeSus,
+    LEXMKChordOptTypeAdd,
+    LEXMKChordOptTypeAltBass
+};
+typedef int LEXMKChordOptType;
+
+/** Chord option with type and related info.
+ 
+ What info means for type?
+ Wide - The widest interval in chord. Info is interval.
+ Sus - suspended chord. Third interval changes to major second or perfect fourth. Make a note, that original third is no more in chord. Info is LEXMKchordSus2 for sus2 and LEXMKchordSus4 for sus4.
+ Add - adding given interval to a chord. Info is interval.
+ AltBass - first note of chord is different from tonic. Info is shift from tonic like -2 for chord Am/G.
+ */
+struct LEXMKChordOpt {
+    LEXMKChordOptType type; /// Type of chord option.
+    int info; /// Info make sense only when type defined.
+};
+typedef struct LEXMKChordOpt LEXMKChordOpt;
+
+LEXMKChordOpt LEXMKChordOptMakeWithTypeAndInfo(LEXMKChordOptType type, int info);
+
+LEXMKChordOpt *LEXMKChordCreateOptsWithTypeFromOpts(LEXMKChordOpt * opts,
+                                                    unsigned int length,
+                                                    LEXMKChordOptType type,
+                                                    unsigned int * outFoundOptsLength);
+
+bool LEXMKChordOptHasOptWithTypeAndInfo(LEXMKChordOpt *opts,
+                                        unsigned int length,
+                                        LEXMKChordOptType type,
+                                        int info);
+
+#endif
